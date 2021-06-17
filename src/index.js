c69941caa1bf85579362bc1b856c924e864c5854
AadityaMuley <aadityamuley8190@gmail.com>
2021-06-17 18:03:44 +0530

    Winner decision logic function added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i=0; i<lines.length; i++) {
+        const [a, b ,c] = lines[i];
+        if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
+
 // class Square extends React.Component {
 //     // constructor(props) {
 //     //     super(props);
@@ -22,7 +45,6 @@ import './index.css';
 //         );
 //     }
 // }
-
 function Square(props) {
     return (
         <button
@@ -46,6 +68,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -63,7 +88,15 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
