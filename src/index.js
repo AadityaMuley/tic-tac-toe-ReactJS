edf15ff2c02c545a3ae177c803ddf0b9cedde178
AadityaMuley <aadityamuley8190@gmail.com>
2021-06-17 17:50:48 +0530

    2 player system added

--- src/index.js
+++ src/index.js
@@ -3,24 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-class Square extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     };
-    // }
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //     super(props);
+//     //     this.state = {
+//     //         value: null,
+//     //     };
+//     // }
 
-    render() {
-        return (
-            <button 
-                className="square" 
-                onClick={ () => this.props.onClick() }
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+//     render() {
+//         return (
+//             <button 
+//                 className="square" 
+//                 onClick={ () => this.props.onClick() }
+//             >
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 
@@ -29,26 +40,30 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
         return (
             <Square
                 value={this.state.squares[i]}
-                onClick={ () => this.handleClick(i) }
+                onClick={() => this.handleClick(i)}
             />
         );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
